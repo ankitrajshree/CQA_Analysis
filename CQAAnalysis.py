c9b910a15a5be0a1f1e8d393ec303c591d69130d
Rishabh Mishra <rishabh.zn200@gmail.com>
2017-11-30 21:45:22 -0700

    Adding comments

--- CQAAnalysis.py
+++ CQAAnalysis.py
@@ -9,6 +9,21 @@ import os
 if __name__ == "__main__":
 
     #Step 0: Check if necessary folders exists
+
+    #Input Folders and Files:
+    #Folder: Data/ai_stackexchange_com
+    #Files to be present:
+    #1. "./Data/ai_stackexchange_com/Badges.xml",
+    #2. "./Data/ai_stackexchange_com/Comments.xml"
+    #3. "./Data/ai_stackexchange_com/PostHistory.xml"
+    #4. "./Data/ai_stackexchange_com/PostLinks.xml"
+    #5. "./Data/ai_stackexchange_com/Posts.xml"
+    #6. "./Data/ai_stackexchange_com/Tags.xml"
+    #7. "./Data/ai_stackexchange_com/Users.xml"
+    #8. "./Data/ai_stackexchange_com/Votes.xml"
+
+
+    #Checks for Output Folder
     OutputDirList = ["CSV Files", "CSV Files_Q", "CSV Files_A", "Images_LTV", "Images_Q", "Images_A"]
     for dir in OutputDirList:
         try:
