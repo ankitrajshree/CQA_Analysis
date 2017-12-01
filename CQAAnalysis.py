211ea17b1e5384f76e99745b72299d57bfa8ea17
Rishabh Mishra <rishabh.zn200@gmail.com>
2017-11-30 21:40:15 -0700

    Adding try..except for checking the output folders.

--- CQAAnalysis.py
+++ CQAAnalysis.py
@@ -4,7 +4,17 @@ import LongTermValue as LTV
 import QuestionTagger as QT
 import PredictQuestionAnswerQuality as PQAQ
 
+import os
+
 if __name__ == "__main__":
+
+    #Step 0: Check if necessary folders exists
+    OutputDirList = ["CSV Files", "CSV Files_Q", "CSV Files_A", "Images_LTV", "Images_Q", "Images_A"]
+    for dir in OutputDirList:
+        try:
+            os.mkdir("./" + dir)
+        except:
+            print(dir + " folder already exists. Output will be overwritten.")
 
     #Step 1: Find Long Term Value of a Question
     ltv = LTV.LongTermValue()
